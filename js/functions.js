7a2350becaaec3bd0587b0a79a50377fe5b9daf5
lynkxyz <lynkxyz@gmail.com>
2016-12-03 01:35:09 +0700

    smooth behavior and watch

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
 
     // scroll fixed menu
     let nav = $('#navmenu');
-    $(document).scroll(function(event) {
+    $(document).scroll(function (event) {
         if ($(this).scrollTop() >= 400) {
             nav.addClass('fix-nav');
             $('#search-bar').addClass('fix-search');
@@ -21,7 +21,7 @@ $(document).ready(() => {
         fontRatio: 30
     });
 
-	$('#showall').flowtype({
+    $('#showall').flowtype({
         minimum: 500,
         maximum: 1200,
         minFont: 12,
@@ -29,6 +29,26 @@ $(document).ready(() => {
         fontRatio: 30
     });
 
+    const animeName = ['Naruto', 'One Piece', 'Fairy Tail', 'Bleach', 'One Punch Man', 'Dragon Ball Super', 'Pokemon', 'Black Rock Shooter', 'Sword Art Online', '5cm/s', 'Date A Live', 'Attack on the Titan', 'Bikini Warriors', 'Tokyo Ghoul', 'Fairy Tail', 'Hunter x Hunter', 'Digimon', 'Gumball', 'Stand By Me', 'Adventure Time', 'Inu Yasha', 'Masou Gakuen HxH', 'Overload', 'God Eater', 'Ajin', 'Gurren Lagann', 'Danmachi', 'Gintama'];
 
+    setInterval(function () {
+        let random = Math.floor(Math.random() * 28) + 1;
+        $('.header').css('background-image', 'url("img/' + random + '.jpg")');
+        $('.header__album h2').html(animeName[random - 1]);
+    }, 5000);
 
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 })
