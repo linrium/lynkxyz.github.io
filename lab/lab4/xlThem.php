90ec2361701b7421cbbe49b213a85963b9c82819
lynkxyz <lynkxyz@gmail.com>
2016-12-03 10:00:23 +0700

    Add files via upload

---
+++ lab/lab4/xlThem.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<?php
+print_r($_REQUEST);
+echo "<br/><br/><br/><br/><br/><br/>";
+print_r($_FILES);
+?>
+</body>
+</html>
