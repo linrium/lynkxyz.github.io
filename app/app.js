20c66cd84af5b123281bf0678107a9fac9bf34de
lynkxyz <lynkxyz@gmail.com>
2016-12-05 10:54:44 +0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -59,6 +59,7 @@ app.controller('NavController', function ($scope, $location, $anchorScroll) {
 });
 
 app.controller('MainController', function ($scope, $rootScope, $state) {
+    console.log($state.current.data.title);
     $rootScope.data = [
         {
             name: "One Piece",
